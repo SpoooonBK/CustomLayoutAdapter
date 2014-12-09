1e537b52b45867a230aac047203f327c7f8e664e
sscsis <erodriguez927@gmail.com>
2014-12-08 19:52:01 -0500

    Initial commit

---
+++ app/src/main/java/myfirstapp/esteban/nyc/customlayoutadapter/CustomArrayAdapter.java
@@ -0,0 +1,37 @@
+package myfirstapp.esteban.nyc.customlayoutadapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.content.Context;
+
+import java.util.ConcurrentModificationException;
+
+/**
+ * Created by sscsis on 12/8/14.
+ */
+public class CustomArrayAdapter extends ArrayAdapter<Post> {
+
+    private Context context;
+    private Post[] posts;
+
+
+
+    public CustomArrayAdapter(Context context, Post[] posts){
+
+        super(context, R.layout.post_layout, posts);
+        this.context = context;
+        this.posts = posts;
+
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater =(LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View postView = inflater.inflate(R.layout.post_layout, parent, false);
+        return postView;
+    }
+}
+
---
+++ app/src/main/java/myfirstapp/esteban/nyc/customlayoutadapter/Post.java
@@ -0,0 +1,7 @@
+package myfirstapp.esteban.nyc.customlayoutadapter;
+
+/**
+ * Created by sscsis on 12/8/14.
+ */
+public class Post {
+}
